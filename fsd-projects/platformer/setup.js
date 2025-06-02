4dbe47d1216e14c4bcf57dd968018f191b19791d
TrentAnderson-coder <TrentAnderson006@gmail.com>
2025-06-02 15:02:29 +0000

    Finalized platformer game

--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -98,5 +98,5 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
-  Diamond: { imgage: "images/collectables/Diamond.png"},
+  Diamond: { image: "images/collectables/Diamond.png"},
 };
